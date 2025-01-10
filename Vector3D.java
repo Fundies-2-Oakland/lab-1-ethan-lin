dee825a659529d16d380f1c968dff8e40d16da19
Ethan Lin <ethanlin2253@gmail.com>
2025-01-10 11:58:39 -0800

    added Vector3D

---
+++ Vector3D.java
@@ -0,0 +1,38 @@
+import java.util.Vector;
+
+    public class Vector3D {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector3D(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public double getZ() {
+            return z;
+        }
+
+        public String toString() {
+            return String.format("(%.2f, %.2f, %.2f)", x, y, z);
+        }
+
+        public static void main(String[] args) {
+        }
+        public double getMagnitude(){
+            return Math.sqrt(x * x + y * y + z * z);
+        }
+
+    }
+
+
